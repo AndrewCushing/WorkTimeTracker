da8a0a48b3061bcb87b31b03a92ef49521f7b825
AndrewCushing <andyc.bt4567@gmail.com>
2019-06-16 15:39:12 +0100

    Changed parameters of the insert record function to take table name and values as I will not always be using the same table so this should not be hardcoded.

--- src/main/java/DBWriter.java
+++ src/main/java/DBWriter.java
@@ -1,15 +1,15 @@
-import javax.swing.plaf.nimbus.State;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.security.PublicKey;
 
 public class DBWriter {
 
-    public static void insertRecord(String name, String date) throws SQLException {
+    public static void insertRecord(String table, String values) throws SQLException {
         Statement stmnt = DBConnector.openConnection();
-        stmnt.executeUpdate("insert into new_table2 values('" + name + "','" + date + "')");
+        stmnt.executeUpdate("insert into " + table + " values(" + values + ")");
         System.out.println("Query sent");
         DBConnector.closeConnection();
     }
+
+
 
 }
